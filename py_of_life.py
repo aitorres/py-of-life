517e4b109afee39b5a0eadad1292627e62bc71c1
Andrés Ignacio Torres <andresitorresm@gmail.com>
2023-01-01 16:12:51 -0400

    chore: minor refactor of generation calculations

--- py_of_life.py
+++ py_of_life.py
@@ -3,6 +3,11 @@ import os
 from dataclasses import dataclass, field
 from typing import Final
 
+DISPLACEMENT_MATRIX: Final[set[tuple[int, int]]] = set(
+    (i, j) for i in [-1, 0, 1] for j in [-1, 0, 1]
+) - {(0, 0)}
+
+
 @dataclass
 class Grid:
     height: int
@@ -18,29 +23,37 @@ class Grid:
             for _ in range(self.width):
                 self.grid[i].append(bool(random.getrandbits(1)))
 
+    def validate_grid_position(self, i: int, j: int) -> bool:
+        return 0 <= i < self.height and 0 <= j < self.width
+
     def compute_next_generation(self) -> None:
         new_grid: list[list[bool]] = []
-        displacement_matrix: list[list[int]] = set(
-            (i, j) for i in [-1, 0, 1] for j in [-1, 0, 1]
-        ) - {(0, 0)}
 
         for i in range(self.height):
             new_grid.append([])
+
             for j in range(self.width):
                 cell = self.grid[i][j]
                 live_neighbors_count = sum(
                     [
                         int(self.grid[i + x][j + y])
-                        for (x, y) in displacement_matrix
-                        if 0 <= (i + x) < self.height
-                        and 0 <= (j + y) < self.width
+                        for x, y in DISPLACEMENT_MATRIX
+                        if self.validate_grid_position(i + x, j + y)
                     ]
                 )
 
+                # Any live cell with two or three live neighbours lives on
+                # to the next generation.
                 if cell and live_neighbors_count in [2, 3]:
                     new_grid[i].append(True)
+                # Any dead cell with exactly three live neighbours becomes a
+                # live cell, as if by reproduction.
                 elif not cell and live_neighbors_count == 3:
                     new_grid[i].append(True)
+                # Any live cell with fewer than two live neighbours dies,
+                # as if by underpopulation.
+                # Any live cell with more than three live neighbours dies,
+                # as if by overpopulation.
                 else:
                     new_grid[i].append(False)
 
